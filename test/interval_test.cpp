ed3b08cbb456fa5824eedf80a24f3b7c591686ce
Franciszek Stokowacki <franek.stokowacki@gmail.com>
2019-04-09 20:12:47 +0200

    remove copy-pasted part

--- test/interval_test.cpp
+++ test/interval_test.cpp
@@ -21,15 +21,3 @@ TEST(IntervalTest, TestSize5) {
 }
 
 }  // namespace
-
-// Step 3. Call RUN_ALL_TESTS() in main().
-//
-// We do this by linking in src/gtest_main.cc file, which consists of
-// a main() function which calls RUN_ALL_TESTS() for us.
-//
-// This runs all the tests you've defined, prints the result, and
-// returns 0 if successful, or 1 otherwise.
-//
-// Did you notice that we didn't register the tests?  The
-// RUN_ALL_TESTS() macro magically knows about all the tests we
-// defined.  Isn't this convenient?
