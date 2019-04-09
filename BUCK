296552c1a9f5016cb3d8aa609927869babeed5ff
Franciszek Stokowacki <franek.stokowacki@gmail.com>
2019-04-09 16:42:53 +0200

    Add gtest and buck

---
+++ BUCK
@@ -0,0 +1,18 @@
+cxx_library(
+  name = 'fvs-algorithms',
+  srcs = [
+    'util/util.cpp'
+  ],
+  headers = [
+    'util/util.h'
+  ],
+  link_style = 'static',
+  deps = [
+  ],
+  visibility = [
+    'PUBLIC',
+  ],
+  compiler_flags = [
+    '-std=c++17',
+  ],
+)
--- util/util.h
+++ util/util.h
@@ -14,76 +14,11 @@ using Graph = vector<vector<int>>;
 
 namespace util
 {
-  namespace 
-  {
-    bool FindCycleDfs(int k, int parent, Graph const& graph, set<int> const& banned, vector<bool>& visited, vector<int>& cycle)
-    {
-      if(visited[k])
-      {
-        cycle.push_back(k);
-        return true;
-      }
+  vector<int> FindCycle(Graph const& graph, set<int> const& banned);
 
-      visited[k] = true;
+  bool IsFvs(Graph const& graph, set<int> const& fvs);
 
-      for(int x : graph[k])
-      {
-        if(banned.count(x) || x == parent)
-          continue;
-        bool res = FindCycleDfs(x, k, graph, banned, visited, cycle);
-        if(res)
-        {
-          if(k == cycle[0])
-            return false;
-          cycle.push_back(k);
-          return true;
-        }
-      }
-      return false;
-    }
-  }
+  void PrintGraph(Graph const& graph);
 
-  vector<int> FindCycle(Graph const& graph, set<int> const& banned)
-  {
-    vector<bool> visited(graph.size(), false);
-    vector<int> cycle;
-
-    for(int i = 0; i < graph.size(); i++)
-    {
-      if(!banned.count(i) && !visited[i])
-      {
-        FindCycleDfs(i, -1, graph, banned, visited, cycle);
-        if(cycle.size() > 0)
-          return cycle;
-      }
-    }
-
-    return cycle;
-  }
-
-  bool IsFvs(Graph const& graph, set<int> const& fvs)
-  {
-    vector<int> const& cycle = FindCycle(graph, fvs);
-    if(cycle.size() == 0)
-      return true;
-    return false;
-  }
-
-  void PrintGraph(Graph const& graph)
-  {
-    for(int i=0; i<graph.size(); i++)
-    {
-      cout << i << ": ";
-      for(int x : graph[i])
-        cout << x << " ";
-      cout << endl;
-    }
-  }
-
-  void PrintVector(vector<int> const& v)
-  {
-    for(int x : v)
-    cerr << x << " ";
-    cerr << endl;
-  }
+  void PrintVector(vector<int> const& v);
 }
